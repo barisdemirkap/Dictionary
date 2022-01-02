1412dae50baa0404127d485269aef666c9b6da42
barisdemirkap <barisdemirkap@gmail.com>
2022-01-02 13:40:16 +0300

    initial commit of dictionary

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic ;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //system.collections.generic
+            Dictionary<int, string> users = new Dictionary<int, string>();
+
+            users.Add(10, "bilbo baggins");
+            users.Add(12, "Aragorn son of arathorn");
+            users.Add(18, "gandalf the grey");
+            users.Add(20, "faramir");
+            // Dizinin elemanlarina erisim
+            Console.WriteLine("***** elemanlara erisim****") ;
+            Console.WriteLine(users[12]) ;
+
+            foreach (var item in users)
+                Console.WriteLine(item) ;
+
+        }
+    }
+}
